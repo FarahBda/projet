c1f4f7a7cb5349bb637cc9bd544c66ad6f82b223
Petouha <rachidgames2.rg@gmail.com>
2023-05-15 14:57:58 +0100

    added page sound effect

--- app/src/main/java/com/example/storyapp/PagerAdapter.java
+++ app/src/main/java/com/example/storyapp/PagerAdapter.java
@@ -2,6 +2,7 @@ package com.example.storyapp;
 
 import android.content.Context;
 import android.media.Image;
+import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import java.util.ArrayList;
 
 public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
+    MediaPlayer mediaPlayer;
     ListData dataArrayList;
     Context context;
     LayoutInflater layoutInflater;
@@ -22,6 +24,7 @@ public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
         this.dataArrayList = dataArrayList;
         this.context = context;
         layoutInflater = LayoutInflater.from(context);
+        mediaPlayer = MediaPlayer.create(context, R.raw.sound2);
     }
 
     @Override
@@ -41,9 +44,16 @@ public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
 
         ImageView img = view.findViewById(R.id.imgPage);
         img.setImageResource(dataArrayList.getStories().get(position).imagePage);
+
 //        TextView txt = view.findViewById(R.id.detailName);
 //        txt.setText(dataArrayList.getStories().get(position).textPage);
         container.addView(view);
+
+
+
+            mediaPlayer.start();
+
+
         return view;
     }
 
